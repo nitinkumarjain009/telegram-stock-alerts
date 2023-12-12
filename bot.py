499ba9ad87142ed86adea03cee6c72faa2b2be8c
Kemal Dahha <kemal-nl@hotmail.com>
2023-12-12 08:22:33 +0100

    Fix delete alert, create function validate_ticker

--- bot.py
+++ bot.py
@@ -30,11 +30,7 @@ def handle_callback_query(call):
     if call.data == "cb_add":
         add_alert(call)
     elif call.data == "cb_delete":
-        bot.send_message(call.from_user.id, "Type the alert ID which you would like to delete.")
-        all_alerts = database.get_all_alerts(connection)
-        bot.send_message(call.from_user.id, all_alerts)
-        bot.register_next_step_handler(call.message, delete_alert_by_id)
-        bot.send_message(call.from_user.id, "Alert has been deleted.")
+        delete_alert(call)
     elif call.data == "cb_show":
         show_all_alerts(call)
 
@@ -42,19 +38,47 @@ def add_alert(call):
     bot.send_message(call.from_user.id, "Type the ticker symbol which you would like to add.")
     bot.register_next_step_handler(call.message, validate_ticker_and_price_data)
 
-def validate_ticker_and_price_data(message):
-    '''Handle the user input for entering a ticker symbol'''
-    # (Re)set attributes for validation
-    validated_ticker_symbol = None
-    validated_price_data = None
+def delete_alert(call):
+    bot.send_message(call.from_user.id, "Type the alert ID which you would like to delete.")
+    show_all_alerts(call)
+    # TODO: should first validate before deleting
+    bot.register_next_step_handler(call.message, validate_id)
 
+def validate_id(message):
+    validated_id = int(message.text) if message.text.isdigit() else None 
+    if validated_id:
+        connection = database.connect()
+        database.delete_alert_by_id(connection, validated_id)
+        bot.send_message(call.from_user.id, "Alert has been deleted.")
+
+def show_all_alerts(call):
+    connection = database.connect()
+    all_alerts = database.get_all_alerts(connection)
+    print(all_alerts)
+    formatted_alerts = [f"{row_num}\t{row[1]}\t{row[2]}" for row_num, row in enumerate(all_alerts)]
+    formatted_alerts = "\n".join(formatted_alerts)
+    bot.send_message(call.from_user.id, formatted_alerts)
+
+def validate_ticker(message):
     # Check for illegal symbols
     if any(s in message.text for s in "., "):
         send_error_message(message, category="ticker symbol", reason="an illegal character")
     else:
+        validated_ticker = message.text.upper()
+        return validated_ticker
+
+def validate_ticker_and_price_data(message):
+    '''Handle the user input for entering a ticker symbol'''
+    # (Re)set attributes for validation
+    validated_ticker = None
+    validated_price_data = None
+
+    # Check for illegal symbols
+    validated_ticker = validate_ticker(message)
+    
+    if validated_ticker:
         # Download price data for valid symbol
-        validated_ticker_symbol = message.text.upper()
-        price_data = get_price_data(validated_ticker_symbol)
+        price_data = get_price_data(validated_ticker)
 
         # Check downloaded price data not empty
         if price_data.empty:
@@ -65,9 +89,9 @@ def validate_ticker_and_price_data(message):
             last_close = validated_price_data["Adj Close"].iloc[-1]
             last_date = validated_price_data.index[-1].date()
                         
-            alert_prompt = f"The last close price for {validated_ticker_symbol} is {last_close:.2f} on {last_date}.\n\nAdd an alert level for {validated_ticker_symbol}. For example:\n130.02\nMA100 (100 daily moving average)\n10%\n-5.5%"
+            alert_prompt = f"The last close price for {validated_ticker} is {last_close:.2f} on {last_date}.\n\nAdd an alert level for {validated_ticker}. For example:\n130.02\nMA100 (100 daily moving average)\n10%\n-5.5%"
             price_message = bot.send_message(message.chat.id, alert_prompt)
-            bot.register_next_step_handler(message, validate_alert_level, validated_ticker_symbol, validated_price_data)
+            bot.register_next_step_handler(message, validate_alert_level, validated_ticker, validated_price_data)
 
 def get_price_data(ticker):
     return yf.download(ticker, period="1y")
@@ -95,17 +119,7 @@ def add_alert_to_database(message, validated_ticker_symbol, validated_alert_leve
     connection = database.connect()
     database.create_table(connection)
     database.add_alert(connection, validated_ticker_symbol, validated_alert_level, last_close)
-    bot.send_message(message.chat.id, f"Successfully added alert for {validated_ticker_symbol} at {validated_alert_level}!")
-
-def show_all_alerts(call):
-    connection = database.connect()
-    all_alerts = database.get_all_alerts(connection)
-    formatted_alerts = "\n".join(map(str, all_alerts))
-    bot.send_message(call.from_user.id, formatted_alerts)
-
-def delete_alert_by_id(message):
-    validated_id = message.text # check if int and in database
-    database.delete_alert_by_id(validated_id)
+    bot.send_message(message.chat.id, f"Successfully added alert for {validated_ticker_symbol} at {validated_alert_level:.2f}!")
 
 def send_error_message(message, category, reason):
     bot.send_message(message.chat.id, f"Invalid {category} due to {reason}! Please enter a valid one.")
@@ -113,83 +127,9 @@ def send_error_message(message, category, reason):
 bot.infinity_polling()
 
 
+# TODO: fix delete functionality
+# TODO: add row_number column to alerts SQL table
 # TODO: format show all alerts as table
 # TODO: format alert_prompt in a nicer way
 # TODO: create check_alerts.py
 # TODO: deploy to Linode
-
-
-
-"""
-    def add_alert(self, call):
-        '''Orchestrates everything to add an alert'''
-        # Send a message prompting user to enter ticker symbol
-        bot.send_message(call.from_user.id, "Type the ticker symbol which you would like to add.")
-        
-        # callback function to set attributes for validated
-        # ticker symbol and price to be used for if condition
-        bot.register_next_step_handler(call.message, self.validate_ticker_and_price_data)
-
-        print(self.validated_ticker_symbol)
-        print(self.validated_price_data)
-
-        # # With validated ticker symbol and price data, proceed 
-        # # to prompt user for entering alert level
-        # if self.validated_ticker_symbol and self.validated_price_data:
-        #     alert_prompt = f"Add an alert level for {self.validated_ticker_symbol}. For example:\n130.02\nMA100 (100 daily moving average)\n10%\n-5.5%"
-        #     price_message = bot.send_message(call.from_user.id, alert_prompt)
-        #     bot.register_next_step_handler(price_message, self.validate_alert_level)
-        
-        # if self.validated_price_data:
-        #     database.add_alert(self.connection, self.validated_ticker_symbol, self.alert_level, self.last_close)
- 
-    def validate_ticker_and_price_data(self, message):
-        '''Handle the user input for entering a ticker symbol'''
-        # (Re)set attributes for validation
-        self.validated_ticker_symbol = None
-        self.validated_price_data = None
-
-        # Check for illegal symbols
-        if any(s in message.text for s in "., "):
-            self.send_error_message(message, category="ticker symbol", reason="an illegal character")
-        else:
-            # Download price data for valid symbol
-            self.validated_ticker_symbol = message.text.upper()
-            price_data = self.get_price_data(self.validated_ticker_symbol)
-
-        # Check downloaded price data not empty
-        if price_data.empty:
-            self.send_error_message(message, category="ticker symbol", reason="a lack of price data")
-        else:
-            self.validated_price_data = price_data
-
-    def validate_alert_level(self, message):
-        ''''''
-        # (Re)set attribute for validation
-        self.alert_level = None
-
-        # Store last close price
-        self.last_close = self.validated_price_data["Adj Close"].iloc[-1]
-        
-        # 
-        if message.text.startswith("MA") and message.text.replace("MA", "", 1).isdigit():
-            self.alert_level = message.text
-        elif message.text.replace("%", "", 1).replace(".", "", 1).replace("-", "", 1).isdigit() and message.text.endswith("%"):
-            self.alert_level = (1+float(message.text.replace("%", "", 1))/100)*self.last_close
-        elif message.text.replace(".", "", 1).isdigit():
-            self.alert_level = float(message.text)
-        else:
-            # Display an error for an invalid alert level
-            self.send_error_message(message.chat.id, category="alert level", reason="a wrong price input")
-        
-
-    
-    def send_error_message(message, category, reason):
-        bot.send_message(message.chat.id, f"Invalid {category} due to {reason}! Please enter a valid one.")
-
-    def delete_alert_by_id(message):
-        ''''''
-        validated_id = message.text # check if int and in database
-        database.delete_alert_by_id(validated_id)
-
-"""
--- readme.md
+++ readme.md
@@ -1,10 +1,13 @@
 # Project Description
-A Telegram bot which can set price alerts for stocks and other securities. Data is pulled from Yahoo Finance, using the [yfinance](https://github.com/ranaroussi/yfinance) library. Library used to create Telegram bot is [pyTelegramBotAPI](https://github.com/eternnoir/pyTelegramBotAPI). Overview of files:
+A Telegram bot which can set price alerts for stocks and other securities. Data is pulled from Yahoo Finance, using the [yfinance](https://github.com/ranaroussi/yfinance) library. Library used to create Telegram bot is [pyTelegramBotAPI](https://github.com/eternnoir/pyTelegramBotAPI). 
+
+Overview of files:
 - bot.py: contains bot logic
 - check_alerts.py: runs once per day to check all the alerts that have been set and trigger alert if condition is met
 - database.py: CRUD functions for sqlite database. Database used for persistent storage.
 
 # How to set it up
 1. Download repository
-1. Create constants.py file in the same directory with `TELEGRAM_TOKEN = "TOKEN"` (replace TOKEN with your API token which you can get following [this](https://core.telegram.org/bots/tutorial#obtain-your-bot-token)
+1. Create constants.py and add the line `TELEGRAM_TOKEN = "TOKEN"` 
+    - Note: replace TOKEN with your API token which you can get following [this](https://core.telegram.org/bots/tutorial#obtain-your-bot-token))
 1. Run app.py
